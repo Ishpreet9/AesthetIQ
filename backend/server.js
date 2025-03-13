553b006d55f130d1e9f10660d8214f24d8f83402
Ishpreet9 <ishpree726@gmail.com>
2025-03-13 19:24:46 +0530

    initiated backend, setup mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import 'dotenv/config'
+import cors from 'cors';
+import connectDB from './config/mongodb.js';
 
+connectDB();
 const PORT = process.env.PORT || 4000
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/',(req,res)=>res.send("API Working"))
 
