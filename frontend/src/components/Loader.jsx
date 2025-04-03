09dc0f1109413330b9b058dccce463e3d2f94ed7
Ishpreet9 <ishpree726@gmail.com>
2025-04-03 18:37:59 +0530

    Loading spinner added

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='w-20 h-20 border-4 border-neutral-400 rounded-full border-t-red-400 animate-spin'></div>
+  )
+}
+
+export default Loader
--- frontend/src/pages/Generate.jsx
+++ frontend/src/pages/Generate.jsx
@@ -2,17 +2,19 @@ import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import axios from 'axios';
 import { AppContext } from '../context/AppContext';
+import Loader from '../components/Loader';
 
 const Generate = () => {
 
   const { backendUrl, image, setImage } = useContext(AppContext);
   const [prompt,setPrompt] = useState('');
   const [style,setStyle] = useState('');
+  const [isLoading,setIsLoading] = useState(false);
 
   const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
     try {
-      e.preventDefault();
-      
       const response = await axios.post(backendUrl + '/api/image/generate-image',{ prompt, style },{ withCredentials: true });
 
       if(response.data.success)
@@ -26,6 +28,8 @@ const Generate = () => {
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -33,9 +37,14 @@ const Generate = () => {
     <div className='flex flex-col gap-6 text-center justify-center items-center mt-12'>
     {/* image section */}
       <div className='bg-neutral-700 w-60 h-60'>
-        {image===''?
-        <p></p>:
-        <img src={image} alt="" className='cursor-pointer' />
+        {isLoading ?
+        <div className='flex justify-center items-center h-full'>
+          <Loader/>
+        </div>
+          : image === '' ?
+          <p></p>
+          :
+          <img src={image} alt="" className='cursor-pointer' />
         }
       </div>
       {/* download and share button */}
@@ -56,26 +65,26 @@ const Generate = () => {
         GENERATE 
       </button>
       <div className='group'>
-      <button className='relative z-0 bg-neutral-900 text-neutral-200 px-10 py-4 rounded-xl border-3 border-black cursor-pointer hover:border-neutral-200 transition-all duration-500 font-semibold min-w-44'>
+      <button type='button' className='relative z-0 bg-neutral-900 text-neutral-200 px-10 py-4 rounded-xl border-3 border-black cursor-pointer hover:border-neutral-200 transition-all duration-500 font-semibold min-w-44'>
         { style && <img src={style === 'anime' ? assets.anime_image : style === 'ghibli' ? assets.ghibli_image : style === 'realistic' ? assets.realistic_image : style === 'logo' ? assets.logo_image : ''} alt="" className='absolute h-full w-full object-cover rounded-xl bottom-0 left-0 opacity-70 z-0'/>}
         <div className='relative z-10'>
         <span>{style === 'anime' ? 'Anime' : style === 'ghibli' ? 'Ghibli-Style' : style === 'realistic' ? 'Realistic' : style === 'logo' ? 'Logo' : 'Style (None)'}</span> 
         </div>
       </button>
       <div className='bg-neutral-900/60 w-96 h-48 absolute bottom-20 left-10 grid grid-cols-2 grid-rows-2 gap-4 p-4 rounded-xl opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-500'>
-        <button onClick={()=>setStyle('anime')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-100 flex justify-center items-center text-center text-xl font-semibold'>
+        <button type='button' onClick={()=>setStyle('anime')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-100 flex justify-center items-center text-center text-xl font-semibold'>
           <img src={assets.anime_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70'/>
           <span className='z-10'>Anime</span>
         </button>
-        <button onClick={()=>setStyle('ghibli')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
+        <button type='button' onClick={()=>setStyle('ghibli')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
           <img src={assets.ghibli_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70'/>
           <span className='z-10'>Ghibli-Style</span>
         </button>
-        <button onClick={()=>setStyle('realistic')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
+        <button type='button' onClick={()=>setStyle('realistic')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
           <img src={assets.realistic_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70' />
           <span className='z-10'>Realistic</span>
         </button>
-        <button onClick={()=>setStyle('logo')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
+        <button type='button' onClick={()=>setStyle('logo')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
           <img src={assets.logo_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70'/>
           <span className='z-10'>Logo</span>
         </button>
