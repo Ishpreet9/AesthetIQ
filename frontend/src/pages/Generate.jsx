2ae5c3fbe08dc29ac06606e3123a44862b77308b
Ishpreet9 <ishpree726@gmail.com>
2025-04-10 15:17:50 +0530

    generate page made responsive

--- frontend/src/pages/Generate.jsx
+++ frontend/src/pages/Generate.jsx
@@ -7,23 +7,22 @@ import Loader from '../components/Loader';
 const Generate = () => {
 
   const { backendUrl, image, setImage } = useContext(AppContext);
-  const [prompt,setPrompt] = useState('');
-  const [style,setStyle] = useState('');
-  const [isLoading,setIsLoading] = useState(false);
+  const [prompt, setPrompt] = useState('');
+  const [style, setStyle] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [showStyles, setShowStyles] = useState(false);
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     setIsLoading(true);
     try {
-      const response = await axios.post(backendUrl + '/api/image/generate-image',{ prompt, style },{ withCredentials: true });
+      const response = await axios.post(backendUrl + '/api/image/generate-image', { prompt, style }, { withCredentials: true });
 
-      if(response.data.success)
-      {
+      if (response.data.success) {
         setImage(response.data.resultImage);
         console.log(response.data.message);
       }
-      else
-      {
+      else {
         console.log("Image not generated!");
       }
     } catch (error) {
@@ -34,8 +33,7 @@ const Generate = () => {
   }
 
   const downloadImage = () => {
-    if(image !== '')
-    {
+    if (image !== '') {
       const link = document.createElement('a');
       link.href = image;
       link.download = 'generatedImage.webp'
@@ -43,70 +41,70 @@ const Generate = () => {
       link.click();
       document.body.removeChild(link);
     }
-    else
-    {
+    else {
       console.log("Image not available to download");
     }
   }
 
   return (
     <div className='flex flex-col gap-6 text-center justify-center items-center mt-12'>
-    {/* image section */}
+      {/* image section */}
       <div className='bg-neutral-700 w-60 h-60'>
         {isLoading ?
-        <div className='flex justify-center items-center h-full'>
-          <Loader/>
-        </div>
+          <div className='flex justify-center items-center h-full'>
+            <Loader />
+          </div>
           : image === '' ?
-          <p></p>
-          :
-          <img src={image} alt="" className='cursor-pointer' />
+            <p></p>
+            :
+            <img src={image} alt="" className='cursor-pointer' />
         }
       </div>
       {/* download and share button */}
       <div className='flex gap-10'>
         <div className='bg-neutral-700 w-13 h-13 rounded-md cursor-pointer flex items-center justify-center'>
-          <img src={assets.download} onClick={downloadImage} alt="" className='w-10 filter invert opacity-60'/>
+          <img src={assets.download} onClick={downloadImage} alt="" className='w-10 filter invert opacity-60' />
         </div>
         <div className='bg-neutral-700 w-13 h-13 rounded-md cursor-pointer flex items-center justify-center'>
           <img src={assets.share} alt="" className='w-8 filter invert opacity-60' />
         </div>
       </div>
-      <form onSubmit={onSubmitHandler} action="" className='flex items-start gap-5 items-center min-h-[160px]'>
-      {/* prompt box */} 
-        <textarea onChange={(e)=>setPrompt(e.target.value)} value={prompt} name="" id="" className='bg-neutral-900 text-white w-150 h-40 border-3 border-black rounded-xl p-2 text-xl overflow-hidden opacity-75 focus:outline-none'></textarea>
-      {/* generate button  */}
-      <div className='flex flex-col h-[160px] justify-around relative'>
-      <button type='submit' className='bg-neutral-900 text-neutral-200 px-10 py-4 rounded-xl border-3 border-black cursor-pointer hover:border-neutral-200 transition-all duration-500 font-semibold'>
-        GENERATE 
-      </button>
-      <div className='group'>
-      <button type='button' className='relative z-0 bg-neutral-900 text-neutral-200 px-10 py-4 rounded-xl border-3 border-black cursor-pointer hover:border-neutral-200 transition-all duration-500 font-semibold min-w-44'>
-        { style && <img src={style === 'anime' ? assets.anime_image : style === 'ghibli' ? assets.ghibli_image : style === 'realistic' ? assets.realistic_image : style === 'logo' ? assets.logo_image : ''} alt="" className='absolute h-full w-full object-cover rounded-xl bottom-0 left-0 opacity-70 z-0'/>}
-        <div className='relative z-10'>
-        <span>{style === 'anime' ? 'Anime' : style === 'ghibli' ? 'Ghibli-Style' : style === 'realistic' ? 'Realistic' : style === 'logo' ? 'Logo' : 'Style (None)'}</span> 
+      <form onSubmit={onSubmitHandler} action="" className='flex md:flex-row flex-col items-start gap-5 items-center min-h-[160px]'>
+        {/* prompt box */}
+        <textarea onChange={(e) => setPrompt(e.target.value)} value={prompt} placeholder='Enter prompt...' name="" id="" className='bg-neutral-900 text-white md:w-150 w-90 h-40 border-3 border-black rounded-xl p-2 text-xl overflow-hidden opacity-75 focus:outline-none'></textarea>
+        {/* generate button  */}
+        <div className='flex md:flex-col flex-row gap-4 md:gap-0 md:h-[160px] h-[60px] justify-around relative'>
+          <button type='submit' className='bg-neutral-900 text-neutral-200 px-10 py-4 rounded-xl border-3 border-black cursor-pointer hover:border-neutral-200 transition-all duration-500 font-semibold'>
+            GENERATE
+          </button>
+          {/* style button */}
+          <div className='group'>
+            <button type='button' onClick={() => setShowStyles(!showStyles)} className='relative z-0 bg-neutral-900 text-neutral-200 px-10 py-4 rounded-xl border-3 border-black cursor-pointer hover:border-neutral-200 transition-all duration-500 font-semibold min-w-44'>
+              {style && <img src={style === 'anime' ? assets.anime_image : style === 'ghibli' ? assets.ghibli_image : style === 'realistic' ? assets.realistic_image : style === 'logo' ? assets.logo_image : ''} alt="" className='absolute h-full w-full object-cover rounded-xl bottom-0 left-0 opacity-70 z-0' />}
+              <div className='relative z-10'>
+                <span>{style === 'anime' ? 'Anime' : style === 'ghibli' ? 'Ghibli-Style' : style === 'realistic' ? 'Realistic' : style === 'logo' ? 'Logo' : 'Style (None)'}</span>
+              </div>
+            </button>
+            <div className={`bg-neutral-900/60 md:w-96 md:h-48 w-80 h-44 absolute md:bottom-18 md:left-10 left-14 bottom-15 grid grid-cols-2 grid-rows-2 gap-4 p-4 rounded-xl ${showStyles ? 'grid' : 'hidden'} md:hidden md:group-hover:grid`}>
+              <button type='button' onClick={() => {setStyle('anime'); setShowStyles(false);}} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-100 flex justify-center items-center text-center text-xl font-semibold'>
+                <img src={assets.anime_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70' />
+                <span className='z-10'>Anime</span>
+              </button>
+              <button type='button' onClick={() => {setStyle('ghibli'); setShowStyles(false);}} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
+                <img src={assets.ghibli_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70' />
+                <span className='z-10'>Ghibli-Style</span>
+              </button>
+              <button type='button' onClick={() => {setStyle('realistic'); setShowStyles(false);}} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
+                <img src={assets.realistic_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70' />
+                <span className='z-10'>Realistic</span>
+              </button>
+              <button type='button' onClick={() => {setStyle('logo'); setShowStyles(false);}} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
+                <img src={assets.logo_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70' />
+                <span className='z-10'>Logo</span>
+              </button>
+            </div>
+          </div>
         </div>
-      </button>
-      <div className='bg-neutral-900/60 w-96 h-48 absolute bottom-20 left-10 grid grid-cols-2 grid-rows-2 gap-4 p-4 rounded-xl opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-500'>
-        <button type='button' onClick={()=>setStyle('anime')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-100 flex justify-center items-center text-center text-xl font-semibold'>
-          <img src={assets.anime_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70'/>
-          <span className='z-10'>Anime</span>
-        </button>
-        <button type='button' onClick={()=>setStyle('ghibli')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
-          <img src={assets.ghibli_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70'/>
-          <span className='z-10'>Ghibli-Style</span>
-        </button>
-        <button type='button' onClick={()=>setStyle('realistic')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
-          <img src={assets.realistic_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70' />
-          <span className='z-10'>Realistic</span>
-        </button>
-        <button type='button' onClick={()=>setStyle('logo')} className='bg-neutral-900 rounded-xl border-3 border-black cursor-pointer relative hover:border-neutral-400 text-neutral-200 flex justify-center items-center text-center text-xl font-semibold'>
-          <img src={assets.logo_image} alt="" className='absolute w-full h-full object-cover rounded-xl opacity-70'/>
-          <span className='z-10'>Logo</span>
-        </button>
-      </div>
-      </div>
-      </div>
       </form>
     </div>
   )
