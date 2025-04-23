5d518f1c7d6ab317e3f5430f76b32486d0f71ff2
Ishpreet9 <ishpree726@gmail.com>
2025-04-23 15:38:54 +0530

    added max age to cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -60,7 +60,8 @@ const loginUser = async (req,res) => {
         res.cookie('token',token,{
             httpOnly: true,
             secure: false,
-            sameSite: "lax"
+            sameSite: "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000
         });
     
         res.status(200).json({success:true, message:'Login Successful', username: user.name});
